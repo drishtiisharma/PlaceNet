907abdd72128fccc6410028706589775886ae341
drishtiisharma <drishti.1106@gmail.com>
2026-06-29 13:34:51 +0530

    shadcn sidebar

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+  title: "PlaceNet",
+  description: "AI Powered Placement Platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("font-sans", inter.variable)}>
+      <body>
+        <TooltipProvider>
+          {children}
+        </TooltipProvider>
+      </body>
+    </html>
+  );
+}
